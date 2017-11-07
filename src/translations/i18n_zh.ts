eb5f74ae5f1f141b0426c0ad350ef76c48f7a805
Harry Chen <i@harrychen.xyz>
2017-11-07 18:29:57 +0800

    Update Chinese translation
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/translations/i18n_zh.ts
+++ src/translations/i18n_zh.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../danmaQ_app.cpp" line="41"/>
         <source>Server: </source>
-        <translation>服务器： </translation>
+        <translation>地址： </translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="47"/>
@@ -21,33 +21,33 @@
     <message>
         <location filename="../danmaQ_app.cpp" line="60"/>
         <source>&amp;Hide</source>
-        <translation>隐藏（&amp;H）</translation>
+        <translation>隐藏(&amp;H)</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="61"/>
         <source>&amp;Config</source>
-        <translation>配置（&amp;C）</translation>
+        <translation>配置(&amp;C)</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="63"/>
         <location filename="../danmaQ_app.cpp" line="172"/>
         <source>&amp;Subscribe</source>
-        <translation>订阅（&amp;S）</translation>
+        <translation>订阅(&amp;S)</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="165"/>
         <source>&amp;Unsubscribe</source>
-        <translation>停止订阅(&amp;U)</translation>
+        <translation>停止(&amp;U)</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="166"/>
         <source>Subscription Started</source>
-        <translation>已经开始订阅</translation>
+        <translation>订阅开始</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="166"/>
         <source>Let&apos;s Go</source>
-        <translation>快试试吧</translation>
+        <translation>快发条弹幕试试吧</translation>
     </message>
     <message>
         <location filename="../danmaQ_app.cpp" line="177"/>
