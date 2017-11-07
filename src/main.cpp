7ffc14d64144d1dafb31df56528c45db14e5ba0c
Jiajie Chen <jiegec@qq.com>
2017-11-07 15:34:21 +0800

    Add i18n for zh

--- src/main.cpp
+++ src/main.cpp
@@ -18,22 +18,47 @@
 #include <QtGlobal>
 #include <QApplication>
 #include <QDebug>
+#include <QTranslator>
 #include <QDesktopWidget>
 #include <ctime>
 #include <cstdlib>
+#include <QDir>
 
 #include "danmaQ_app.h"
+#include "danmaku.h"
 
 #if defined NDEBUG && (defined _WIN32 || defined __CYGWIN__)
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 int main(int argc, char *argv[])
 {
+    Q_INIT_RESOURCE(i18n);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QApplication app(argc, argv);
     std::srand(std::time(0));
+
+	QDir dir(":translations");
+	QStringList fileNames = dir.entryList(QStringList("*.qm"), QDir::Files,
+										  QDir::Name);
+	QStringListIterator i(fileNames);
+	QString defaultLocale = QLocale::system().name();
+	defaultLocale.truncate(defaultLocale.lastIndexOf('_'));
+	myDebug << defaultLocale;
+	while (i.hasNext()) {
+		QString file = i.next();
+		QString current = file;
+		current.truncate(current.lastIndexOf('.'));
+		current.remove(0, current.indexOf('_') + 1);
+		if (defaultLocale == current) {
+			myDebug << "Using translation file " << dir.filePath(file);
+			QTranslator *translator = new QTranslator();
+			translator->load(dir.filePath(file));
+			app.installTranslator(translator);
+			break;
+		}
+	}
 
 	QDesktopWidget* desktop = QApplication::desktop();
 	DMApp* dm_app = new DMApp();
