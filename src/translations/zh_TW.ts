e570f41dc2e80bf448faeb9bf6b99245fc9a591b
Harry Chen <i@harrychen.xyz>
2017-11-08 15:39:26 +0800

    Add translation for zh_TW This commit fixed #28

---
+++ src/translations/zh_TW.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>DMApp</name>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="41"/>
+        <source>Server: </source>
+        <translation>地址： </translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="47"/>
+        <source>Channel: </source>
+        <translation>頻道： </translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="53"/>
+        <source>Password: </source>
+        <translation>密碼： </translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="60"/>
+        <source>&amp;Hide</source>
+        <translation>隱藏(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="61"/>
+        <source>&amp;Config</source>
+        <translation>配置(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="63"/>
+        <location filename="../danmaQ_app.cpp" line="172"/>
+        <source>&amp;Subscribe</source>
+        <translation>訂閱(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="165"/>
+        <source>&amp;Unsubscribe</source>
+        <translation>停止(&amp;U)</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="166"/>
+        <source>Subscription Started</source>
+        <translation>訂閱開始</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="166"/>
+        <source>Let&apos;s Go</source>
+        <translation>快發條彈幕試試吧</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="177"/>
+        <source>Ooops!</source>
+        <translation>哎呀！</translation>
+    </message>
+</context>
+<context>
+    <name>DMTrayIcon</name>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="209"/>
+        <source>Toggle Subscription</source>
+        <translation>切換訂閱狀態</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="210"/>
+        <source>Refresh Screen</source>
+        <translation>刷新屏幕</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="211"/>
+        <source>Show Main Window</source>
+        <translation>顯示主窗口</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="212"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="../danmaQ_app.cpp" line="213"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+</context>
+</TS>
--- update_i18n.sh
+++ update_i18n.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export PATH=/usr/local/opt/qt/bin:$PATH
 lupdate -recursive . -ts src/translations/zh_CN.ts
+lupdate -recursive . -ts src/translations/zh_TW.ts
 lupdate -recursive . -ts src/translations/ja_JP.ts
