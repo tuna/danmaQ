fdaa0f26e1497feca0d10c4f2feb16f087825a36
Shengqi Chen <harry-chen@outlook.com>
2025-03-25 12:14:10 +0800

    Define #NOMINMAX to fix error in MSVC
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- src/Danmaku.cpp
+++ src/Danmaku.cpp
@@ -15,6 +15,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#if defined _MSC_VER
+#define NOMINMAX
+#endif
+
 #include <QtCore>
 #include <QApplication>
 #include <QLabel>
