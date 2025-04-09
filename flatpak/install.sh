19fe7cea3b3369439c0683952069b17a063bee64
papersnake <prnake@gmail.com>
2025-04-09 17:32:03 +0000

    feat: update flatpak build version

--- flatpak/install.sh
+++ flatpak/install.sh
@@ -1,5 +1,5 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.kde.Sdk//5.15
-flatpak install flathub io.qt.qtwebengine.BaseApp//5.15
+flatpak install flathub org.kde.Sdk//6.8
+flatpak install flathub io.qt.qtwebengine.BaseApp//6.8
 flatpak-builder --repo=repo --force-clean build-dir org.tuna.danmaQ.local.json
 flatpak build-bundle repo danmaQ.flatpak org.tuna.danmaQ
